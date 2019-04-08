e5d8f1ff8c747de3a927bc61f4c6e29444ba9d68
BSO <roksolar999@gmail.com>
2019-04-08 19:20:27 +0200

    first commit

---
+++ build/open_esplibs_libpp/pp.d
@@ -0,0 +1,23 @@
+build/open_esplibs_libpp//pp.o: \
+ /home/bso/esp-open-rtos/open_esplibs/libpp/pp.c \
+ ../../open_esplibs/include/open_esplibs.h \
+ ../../libc/xtensa-lx106-elf/include/stdlib.h \
+ ../../libc/xtensa-lx106-elf/include/machine/ieeefp.h \
+ ../../libc/xtensa-lx106-elf/include/_ansi.h \
+ ../../libc/xtensa-lx106-elf/include/newlib.h \
+ ../../libc/xtensa-lx106-elf/include/_newlib_version.h \
+ ../../libc/xtensa-lx106-elf/include/sys/config.h \
+ ../../libc/xtensa-lx106-elf/include/sys/features.h \
+ ../../libc/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ ../../libc/xtensa-lx106-elf/include/sys/reent.h \
+ ../../libc/xtensa-lx106-elf/include/_ansi.h \
+ ../../libc/xtensa-lx106-elf/include/sys/_types.h \
+ ../../libc/xtensa-lx106-elf/include/machine/_types.h \
+ ../../libc/xtensa-lx106-elf/include/machine/_default_types.h \
+ ../../libc/xtensa-lx106-elf/include/sys/lock.h \
+ ../../libc/xtensa-lx106-elf/include/sys/cdefs.h \
+ ../../libc/xtensa-lx106-elf/include/machine/stdlib.h \
+ ../../libc/xtensa-lx106-elf/include/alloca.h \
+ ../../libc/xtensa-lx106-elf/include/stdint.h \
+ ../../libc/xtensa-lx106-elf/include/sys/_intsup.h \
+ ../../libc/xtensa-lx106-elf/include/sys/_stdint.h
