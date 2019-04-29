90545e8bc0b7b9f86d93dcbb20bca745dcc26747
bm0383 <bm0383@student.uni-lj.si>
2019-04-29 10:12:16 +0200

    Added client.py

--- client.py
+++ client.py
@@ -1,26 +1,48 @@
 import paho.mqtt.client as mqtt #import the client1
-import time
+import time as t
+import sqlite3
+import datetime
+
+
 ############
+
 def on_message(client, userdata, message):
+    sqlite_file = 'sensordata.db'
+    # Connecting to the database file
+    conn = sqlite3.connect(sqlite_file)
+    c = conn.cursor()
     print("-----------------------------------------------")
-    print("message received " ,str(message.payload.decode("utf-8")))
+    print("message recekkived " ,str(message.payload.decode("utf-8")))
     print("message topic=",message.topic)
     print("message qos=",message.qos)
-    print("message retain flag=",message.retain)
+    print("message retain flag=",message.retain)    
+    pressure = str(message.payload.decode("utf-8"))
+    device_name = message.topic
+
+    try:
+        today = datetime.datetime.now()
+        c.execute('''INSERT INTO readings(pressure,currentdate,device) VALUES(?,?,?)''', (pressure,today,device_name,))
+        conn.commit()
+        print("KONEC")
+    except Exception as e:
+        print(e)
+        print("BAZA")
     
 ########################################
 broker_address="localhost"
-#broker_address="iot.eclipse.org"
+#broker_address="test.mosquitto.org"
 print("creating new instance")
 client = mqtt.Client("P1") #create new instance
 client.on_message=on_message #attach function to callback
 print("connecting to broker")
 client.connect(broker_address) #connect to broker
 client.loop_start() #start the loop
-print("Subscribing to topic", "/BSO-pressure")
-client.subscribe("/BSO-pressure")
+topic = "/BSO-1"
+print("Subscribing to topic", topic)
+client.subscribe(topic)
+topic = "/BSO-2"
+print("Subscribing to topic", topic)
+client.subscribe(topic)
+
 while True:
-    #print("Publishing message to topic","house/bulbs/bulb1")
-    #client.publish("house/bulbs/bulb1","OFF")
-    time.sleep(1) # wait
-    #client.loop_stop() #stop the loop
+    t.sleep(1) # wait
